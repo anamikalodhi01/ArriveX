ac23b9a51f6ed7769f07e1868da8988d67ef9205
Anamika <anamikalodhi77@gmail.com>
2026-02-02 18:07:57 +0530

    feat: add authentication-aware navigation bar

--- frontend/src/components/Layout/Navbar.jsx
+++ frontend/src/components/Layout/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
     <nav className="navbar">
       <div className="navbar-container">
         <Link to="/" className="navbar-brand">
-          🚆 Smart Travel Alert
+          - Smart Travel Alert
         </Link>
         <div className="navbar-links">
           {user ? (
